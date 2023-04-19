c6ee0a5abecfbebcdfd0d68d0ddd40a297d4a480
yurii19 <yurikolechkin@gmail.com>
2023-04-19 13:23:11 +0300

    chore: header component added

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,28 @@
 <template>
-  <img alt="Vue logo" src="https://s.dou.ua/CACHE/images/img/static/companies/panda_logo/bfe851809828be6e25ca3574a2517c54.png">
- <MainApp />
+  <img
+    alt="Panda logo"
+    width="150"
+    src="https://s.dou.ua/CACHE/images/img/static/companies/panda_logo/bfe851809828be6e25ca3574a2517c54.png"
+  />
+  <HeaderApp />
+  <MainApp />
+  
 </template>
 
 <script>
 //import HelloWorld from './components/HelloWorld.vue'
-import MainApp from './layouts/MainApp.vue'
+import MainApp from "./layouts/MainApp.vue";
+import HeaderApp from "./components/HeaderApp.vue";
 //import HelloWorld from './layouts/MainApp.vue.vue'
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
     // HelloWorld,
-    MainApp
-  }
-}
+    MainApp,
+    HeaderApp
+  },
+};
 </script>
 
 <style>
--- src/components/HeaderApp.vue
+++ src/components/HeaderApp.vue
@@ -1,16 +1,40 @@
 <template lang="html">
-  <section class="home">
-    <h1>home Component</h1>
-  </section>
+  <div class="header">
+    <div class="menu-nav">
+      <ul class="nav-list">
+        <li>
+          <a href="#">Main</a>
+        </li>
+        <li>
+          <a href="#">Selected</a>
+        </li>
+      </ul>
+    </div>
+  </div>
 </template>
- 
+
 <script>
 //import { Options, Vue } from "vue-class-component";
 // @Options({})
-export default{}
+export default {};
 </script>
- 
-<style scoped >
-/* .home {
-} */
+
+<style scoped>
+.header {
+  background-color: antiquewhite;
+}
+.nav-list {
+  display: flex;
+  /* justify-content: space-around; */
+  list-style-type: none;
+  /* width: 20%; */
+}
+.nav-list > li {
+  margin: 0 20px 0 0;
+}
+.nav-list > li > a {
+  /* border: 1px solid blue; */
+  text-decoration: none;
+  font-size: 1.2em;
+}
 </style>
